a9bd2bf1abc35c26bd7b3108c98a54f806d5fbb1
Laura Phillips <laura_phillips@rapid7.com>
2022-06-14 14:10:47 +0100

    Adding Application Java Source

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -9,6 +9,6 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	    Application app = new Application();
+	Application app = new Application();
     }
 }
